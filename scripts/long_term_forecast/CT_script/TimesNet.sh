94f97eb9548193427d1f8f2e8ab1067225f4fbd4
Stephen Rogers <u6949364@anu.edu.au>
2024-02-21 17:42:11 +1100

    runs on ct dataset

---
+++ scripts/long_term_forecast/CT_script/TimesNet.sh
@@ -0,0 +1,105 @@
+export CUDA_VISIBLE_DEVICES=5
+
+model_name=TimesNet
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/CT/ \
+  --data_path ct_01_weather_15_subset.csv \
+  --model_id ct_96_384 \
+  --model $model_name \
+  --data custom \
+  --features MS \
+  --target ct_01 \
+  --freq 15min \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 384 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_epoch 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/weather/ \
+#   --data_path weather.csv \
+#   --model_id weather_96_192 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 21 \
+#   --dec_in 21 \
+#   --c_out 21 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 5 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --train_epochs 1
+
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/weather/ \
+#   --data_path weather.csv \
+#   --model_id weather_96_336 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 21 \
+#   --dec_in 21 \
+#   --c_out 21 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 5 \
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/weather/ \
+#   --data_path weather.csv \
+#   --model_id weather_96_720 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 21 \
+#   --dec_in 21 \
+#   --c_out 21 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 5 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --train_epochs 1
